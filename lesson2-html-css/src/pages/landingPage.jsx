c729fefd527cde207377aedc46c1274ce422ca51
vuongtanphat139 <73147318+vuongtanphat139@users.noreply.github.com>
2024-04-08 10:01:37 +0700

    update icon feature

--- lesson2-html-css/src/pages/landingPage.jsx
+++ lesson2-html-css/src/pages/landingPage.jsx
@@ -42,13 +42,48 @@ export default function LandingPage() {
           <span className="feature-heading-color">we can do</span>
         </h2>
         <div className="feature-list">
-          {items.map((item) => (
-            <div className="feature-item" key={item.id}>
-              <FontAwesomeIcon icon={faLaptop} className="icon" />
-              <h3>{item.title}</h3>
-              <p>{item.content}</p>
+            <div className="feature-item">
+              <div className="container-icon">
+                <img alt="" src="https://www.cyberlogitec.com.vn/_next/image?url=%2Fstatic%2Fimages%2Fservices%2Fvector1.png&w=64&q=75" className="icon"></img>
+              </div>
+              <h3>Customer’s Requirements Managed By Help Desk Software.</h3>
+              <p>Enable to provide quick and easy answers to help seekers. Makes it simple to organize, prioritize.</p>
             </div>
-          ))}
+            <div className="feature-item">
+              <div className="container-icon">
+                <img alt="" src="https://www.cyberlogitec.com.vn/_next/image?url=%2Fstatic%2Fimages%2Fservices%2Fvector2.png&w=96&q=75" className="icon"></img>
+              </div>
+              <h3>Deliver On SLAs</h3>
+              <p>Nail your Service Level Agreements, every time. Get the important things done first with prioritized queues.</p>
+            </div>
+            <div className="feature-item">
+              <div className="container-icon">
+                <img alt="" src="https://www.cyberlogitec.com.vn/_next/image?url=%2Fstatic%2Fimages%2Fservices%2Fvector3.png&w=64&q=75" className="icon"></img>
+              </div>
+              <h3>Offer Self-Service</h3>
+              <p>Help customers help themselves, scale support with a limited staff. Save time and reduce tickets with an integrated knowledge base.</p>
+            </div>
+            <div className="feature-item">
+              <div className="container-icon">
+                <img alt="" src="https://www.cyberlogitec.com.vn/_next/image?url=%2Fstatic%2Fimages%2Fservices%2Fvector4.png&w=64&q=75" className="icon"></img>
+              </div>
+              <h3>Automate Those Repetitive Tasks</h3>
+              <p>We always try to set up automations for the routine tasks so that we can focus on solving the important stuff and help lighten the workload.</p>
+            </div>
+            <div className="feature-item">
+              <div className="container-icon">
+                <img alt="" src="https://www.cyberlogitec.com.vn/_next/image?url=%2Fstatic%2Fimages%2Fservices%2Fvector5.png&w=64&q=75" className="icon"></img>
+              </div>
+              <h3>Customer’s Requirements Tracked And Reported On Metrics</h3>
+              <p>We definitely keep an eye on key operational metrics like most IT organizations. But we’ve stopped obsessing over random KPIs, and we’re way more focused now on measuring what matters most. To summarize, we put the customer experience first, and spend our time drilling into the trends and numbers that can help us make the biggest improvements.</p>
+            </div>
+            <div className="feature-item">
+              <div className="container-icon">
+                <img alt="" src="https://www.cyberlogitec.com.vn/_next/image?url=%2Fstatic%2Fimages%2Fservices%2Fvector6.png&w=64&q=75" className="icon"></img>
+              </div>
+              <h3>Up-To-Date Staff Training</h3>
+              <p>CLV helpdesk’s tool is always available with the up to date workaround, training materials, guideline document, FAQ from users… We had a clear plan to make sure all of helpdesk team members could have the easiest way to reach them in the fastest way when they need to.</p>
+            </div>
         </div>
       </div>
 
